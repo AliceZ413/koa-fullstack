69a0dfa70be0c5b18625a08d2bac1a2ea6d3c58c
AndyZ <zhangy01@softin-tech.com>
2023-12-13 15:44:37 +0800

    fix: fixed some bug

--- views/App.vue
+++ views/App.vue
@@ -11,7 +11,26 @@
       </ul>
     </nav>
     <main>
+      <div>{{ count }}</div>
+      <button @click="handleClick">click me</button>
       <RouterView />
     </main>
   </div>
 </template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue';
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const count = ref(0);
+    const handleClick = () => {
+      count.value++;
+    };
+    return {
+      count,
+      handleClick,
+    };
+  },
+});
+</script>
